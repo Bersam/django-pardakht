0856933648a5bdc59b793426a5652cbea47e9ae3
Bersam Karbasion <bersam.k@gmail.com>
2019-05-02 17:56:58 +0430

    modify django admin panel

--- pardakht/admin.py
+++ pardakht/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from pardakht.models import Payment
 
-
-admin.site.register(Payment)
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('price', 'state', 'user', 'gateway', 'payment_result', 'verification_result','token', 'created_at')
+    list_filter = ('gateway', 'state')
+    search_fields = ('token', )
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
