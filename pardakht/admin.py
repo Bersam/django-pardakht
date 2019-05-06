00fff0c2e77df57666d2bcdfcb7676a822269162
Bersam Karbasion <bersam.k@gmail.com>
2019-05-06 17:21:21 +0430

    add ref_number to admin panel

--- pardakht/admin.py
+++ pardakht/admin.py
@@ -3,8 +3,9 @@ from pardakht.models import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('price', 'state', 'user', 'gateway', 'payment_result', 'verification_result','token', 'created_at')
-    list_filter = ('gateway', 'state')
-    search_fields = ('token', )
+    list_display = ('ref_number', 'price', 'state', 'user', 'gateway', 'payment_result', 'verification_result','token', 'created_at')
+    # add user to list_filter and search_fields
+    list_filter = ('gateway', 'state',)
+    search_fields = ('token', 'ref_number')
     ordering = ('-created_at',)
     date_hierarchy = 'created_at'
